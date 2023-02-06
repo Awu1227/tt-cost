3ee96ab90a7bc83de1e47426a20e9d9d52f6a6a4
orange <2497436726@qq.com>
2023-02-06 22:21:09 +0800

    :bug: change the route jump

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -1,8 +1,8 @@
 <template>
 	<van-tabbar v-model="active" active-color="#AD8B73">
 		<van-tabbar-item icon="home-o" @click="routerPush('/home')">首页</van-tabbar-item>
-		<van-tabbar-item icon="bar-chart-o" @click="routerPush('/data')">统计</van-tabbar-item>
-		<van-tabbar-item icon="manager-o" @click="routerPush('/user')">我的</van-tabbar-item>
+		<van-tabbar-item icon="bar-chart-o" @click="routerPush('/cost-statistics')">统计</van-tabbar-item>
+		<van-tabbar-item icon="manager-o" @click="routerPush('/user-info')">我的</van-tabbar-item>
 	</van-tabbar>
 </template>
 
